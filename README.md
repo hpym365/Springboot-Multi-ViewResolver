fb81fd2209a88c5de252e6f4f79349e5eb7d7ed3
hpym365 <66033086@qq.com>
2017-05-12 00:42:01 +0800

    readme

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ visit http://localhost:8888/test   resolve by  InternalResourceViewResolver cont
 visit http://localhost:8888/test.xml   resolve by  ThymeleafViewResolver   contenttype TEXT_XML  --xml
 visit http://localhost:8888/test.html   resolve by  ThymeleafViewResolver contenttype APPLICATION_XHTML_XML   --html 
 visit http://localhost:8888/test.json   resolve by  MappingJackson2JsonView contenttype APPLICATION_JSON   --json
+
+
+###### 1、外部系统通过http调用排队叫号系统(his 核医学等外部业务系统)，
+2、数据过来之后，通过字典表redis缓存，
+先保存redis(使用redis的list集合保存每个科室的队列情况?-通过系统key+医院key+院区key+科室key) 
+保存超时或者redis挂掉(超时设置?) 
+之后保存数据库.
+2、在redis无法使用时候保证系统可以使用数据库正常工作。
+3、超时打印日志，使用apache storm进行日志分析。
+4、设计表不光考虑到科室 院区 还考虑到医院 名称  为区域化设计做准备；
+5、患者标识设计 急诊 军？ vip？不应设置不同字段flag 
+不方便扩展，使用一个字段type无法支持多状态 即急诊又vip又军人
+6、患者被叫过之后数据应落地 加入到mq消息队列(mq问题时容错?) 
+做持久化或数据库已被查询等？
+mq解耦 系统在拆分？ 业务处理？  查询处理系统？
+7、
+7、管理者领导查看院区队列情况？不同权限查看不同级别 医院-院区-科室-...
+
+springboot + vue? or thymeleaf? + druid + mysql?+oracle + 
+rabbitmq + redis + swgger2? + 
+
+入口考虑多支持? http 插库 socket连接? webservice?
+
+排队叫号终端支持?
+sQueue
--- src/main/java/com/senyint/ctrl/TestController.java
+++ src/main/java/com/senyint/ctrl/TestController.java
@@ -1,9 +1,17 @@
 package com.senyint.ctrl;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by hanpeng on 2017/4/17.
@@ -11,9 +19,46 @@ import java.util.Map;
 @Controller
 public class TestController {
 
+    private RabbitTemplate rabbitTemplate;
+
     @RequestMapping(value = "/test")
     public String Test(Map<String, Object> map){
+        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend(this.EXCHANGE, this.ROUTINGKEY, "abcdefg", correlationId);
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean b, String s) {
+                if(b){
+                    System.out.println("发送成功!");
+                }
+            }
+        });
         map.put("json","test");
         return "test";
     }
+
+    public static final String EXCHANGE   = "spring-boot-exchange";
+    public static final String ROUTINGKEY = "spring-boot-routingKey";
+
+
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses("127.0.0.1:5672");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true); //必须要设置
+        return connectionFactory;
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    //必须是prototype类型
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
+
 }
